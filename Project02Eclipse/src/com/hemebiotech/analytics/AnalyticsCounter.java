f0a7f33fd5eac7ac959d793e37b32b499c5270af
ernhollam <ernholla.marinasy@gmail.com>
2021-11-08 12:00:08 +0100

    Update javadoc for AnalyticsCounter and add javadoc in CountSymptomDataFromFile

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,23 +3,54 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
-/** Outputs a file with symptoms ordered alphabetically and their occurrences
+/**<p><b>Outputs a file with symptoms ordered alphabetically and their occurrences</b>
+ * <br>
  *
- * Analyses side effects trend to make a prediction in order to improve our products.
- * It works with a .txt file with all symptoms and their duplicates.
- * It outputs an .out file with all the symptoms and how many times they occurred during the experience.
- *
- * AnalyticsCounter is made of three steps:
- * 		- Read a simple text file with symptoms and their duplicates
- * 		- Analyse the symptoms file and count how many times they appeared
- * 		- Write the symptoms alphabetically and the number of occurrences in a .out file
- *
+ * Analyses side effects trend to make a prediction in order to improve our products.<br>
+ * It works with a .txt file with all symptoms and their duplicates.<br>
+ * It outputs an .out file with all the symptoms and how many times they occurred during the experience.<br>
+ *<ul>  <b>AnalyticsCounter</b> is made of three steps:
+ * 	<li>Read a simple text file with symptoms and their duplicates</li>
+ * 	<li>Analyse the symptoms file and count how many times they appeared</li>
+ * 	<li>Write the symptoms alphabetically and the number of occurrences in a .out file</li>
+ *</ul>
+ * </p>
  * @author Ernholla MARINASY
+ *
+ * @see ISymptomReader
+ * @see ISymptomCounter
+ * @see ISymptomWriter
+ * @see ReadSymptomDataFromFile
+ * @see CountSymptomDataFromFile
+ * @see WriteSymptomDataFromFile
  */
 public class AnalyticsCounter {
 
-	/** Main method for AnalyticsCounter
-	 *
+	/** <b>Main method for AnalyticsCounter</b>
+	 * <br>
+	 * Requires the presence of a <i>symptoms.txt</i> file in the project folder.
+	 * <p>In order to output the symptoms alphabetically, <b>AnalyticsCounter</b> is composed of three steps:
+	 * <ol>
+	 *     <li>Read the symptoms.txt file and output a <i>List</i> variable with all symptoms and their duplicates</li>
+	 *     <li>Analyse the <i>List</i> variable, count the symptoms and output a <i>TreeMap</i> (self ordering Collection) to
+	 *     order symptoms alphabetically, the <i>TreeMap</i>'s key is the symptom and its value represents how many times it occurred</li>
+	 *     <li>Write the <i>TreeMap</i> content into a <i>result.out</i> file</li>
+	 * </ol></p>
+	 * <br>
+	 * The <i>result.out</i> file appears as such: <br>
+	 * anxiety 5<br>
+	 * arrhythmias 3<br>
+	 * ...
+	 * <br><br>
+	 * @author Ernholla MARINASY
+	 *<br>
+	 * @see AnalyticsCounter
+	 * @see ISymptomReader
+	 * @see ISymptomCounter
+	 * @see ISymptomWriter
+	 * @see ReadSymptomDataFromFile
+	 * @see CountSymptomDataFromFile
+	 * @see WriteSymptomDataFromFile
 	 */
 	public static void main(String... args) throws Exception {
 		// Create local variables for all function outputs
--- Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomDataFromFile.java
@@ -4,33 +4,57 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-/**
- * Use loops to count the symptoms from a List variable
+/**<p> <b>A counter that can count and order symptoms alphabetically from a file</b><br>
+ * Implements ISymptomCounter interface<br>
+ * Uses two for-loops to count the symptoms from a <i>List</i> variable<br>
+ * Has one method <i>countSymptoms</i><br><br>
+ * </p>
+ * @author Ernholla MARINASY
  *
+ * @see com.hemebiotech.analytics.ISymptomCounter
+ * @see CountSymptomDataFromFile#CountSymptomDataFromFile(List)
+ * @see CountSymptomDataFromFile#countSymptoms
  */
 public class CountSymptomDataFromFile implements ISymptomCounter {
 
 	private final List<String> symptomsList;
 
-	/**
+	/**<b>Constructor for CountSymptomDataFromFile</b>
 	 *
 	 * @param symptomsList a List<String> of all symptoms found in file
+	 *<br><br>
+	 * @see CountSymptomDataFromFile
 	 */
 	public CountSymptomDataFromFile(List<String> symptomsList) {
 		this.symptomsList = symptomsList;
 	}
 
+	/**<b>Counts symptoms read from file and return an alphabetically ordered list of symptoms</b>
+	 * <br>
+	 * <p>The secret lies in the use of a <i>TreeMap</i>, an efficient means of storing key-value pairs in sorted order.<br>
+	 * The keys maintained in the map are the symptoms or side effects, the values mapped are the side effects' occurrences.<br>
+	 * <br>
+	 * <b>countSymptoms</b> works as follows:
+	 * <ol>
+	 *     <li>Check if the specified <i>List</i> is empty, if so, return an empty <i>TreeMap</i></li>
+	 *     <li>If the <i>List</i> contains symptoms, browse it, use one symptom at a time.
+	 *     First see if the symptom has already been counted, then browse the <i>List</i> again and count how many times it appears</li>
+	 * </ol>
+	 * </p>
+	 * @return A TreeMap with all symptoms and their occurrences
+	 *<br><br>
+	 * @see AnalyticsCounter
+	 * @see ISymptomCounter
+	 * @see CountSymptomDataFromFile
+	 */
 	@Override
 	public Map<String, Integer> countSymptoms() {
 		if (symptomsList.size()==0){
-			// If provided list is empty, return empty TreeMap
 			return new TreeMap<>();
 		}
 		else {
-			// Browse list and count symptoms
 			Map<String, Integer> result = new TreeMap<>();
 			for (int indexList = 0; indexList < symptomsList.size(); indexList++) {
-				// Initialize number of occurrence of selected symptom
 				int nbOccurrencesOfSymptom = 0;
 				// Create temporary variable for the symptom to count
 				String currentSymptom = symptomsList.get(indexList);
