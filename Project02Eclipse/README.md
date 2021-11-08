40419a8b632d7f6f22de92cdc2ee96078d955a4a
ernhollam <ernholla.marinasy@gmail.com>
2021-11-08 10:53:24 +0100

    Add JavaDoc for AnalyticsCounter and update readme file for project

--- Project02Eclipse/README.md
+++ Project02Eclipse/README.md
@@ -1,1 +1,13 @@
-Sample project for OpenClassroom's fundamentals of Java
+# Sample project for OpenClassroom's fundamentals of Java
+
+## HEME Biotech: Trend analysis to predict customer needs
+
+This programm analyses symptoms trend to make a prediction in order to improve our produtcts.
+It works with a .txt file with all symptoms listed and outputs an .out file with all the symptoms and how many times they occurred during the experience.
+
+AnalyticsCounter is made of three step:
+ - Read a simple text file with symptoms listed with duplicates
+ - Analyse the symptoms file and count how many times they appeared
+ - Write the symptoms alphabetically and the number of occurrences in a .out file
+ 
+ 
--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,14 +3,30 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
+/** Outputs a file with symptoms ordered alphabetically and their occurrences
+ *
+ * Analyses side effects trend to make a prediction in order to improve our products.
+ * It works with a .txt file with all symptoms and their duplicates.
+ * It outputs an .out file with all the symptoms and how many times they occurred during the experience.
+ *
+ * AnalyticsCounter is made of three steps:
+ * 		- Read a simple text file with symptoms and their duplicates
+ * 		- Analyse the symptoms file and count how many times they appeared
+ * 		- Write the symptoms alphabetically and the number of occurrences in a .out file
+ *
+ * @author Ernholla MARINASY
+ */
 public class AnalyticsCounter {
 
+	/** Main method for AnalyticsCounter
+	 *
+	 */
 	public static void main(String... args) throws Exception {
 		// Create local variables for all function outputs
 		List<String> symptomsReadFromFile;
 		Map<String,Integer> symptomsWithOccurrences;
 
-		// Create instance of symptom reader
+		// Read symptoms from specified file
 		ISymptomReader listFromSymptomReader = new ReadSymptomDataFromFile("symptoms.txt");
 		symptomsReadFromFile = listFromSymptomReader.getSymptoms();
 
@@ -18,7 +34,7 @@ public class AnalyticsCounter {
 		ISymptomCounter symptomsListFromFile = new CountSymptomDataFromFile(symptomsReadFromFile);
 		symptomsWithOccurrences = symptomsListFromFile.countSymptoms();
 
-		// Write symptoms in specified file
+		// Write symptoms in result.out
 		ISymptomWriter myFileWriter = new WriteSymptomDataFromFile("result.out", symptomsWithOccurrences);
 		myFileWriter.writeSymptoms();
 	}
